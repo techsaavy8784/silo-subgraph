79465afd9ec82519210663553e5ee43fc43f896e
techsaavy8784 <blueskydev1205@gmail.com>
2021-10-06 20:38:34 -0400

    updated factory address

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,11 +4,11 @@ import { Silo } from '../generated/schema';
 import { NewSilo } from '../generated/SiloFactory/SiloFactory';
 
 /*
-id: ID!
+  id: ID!
   name: String! 
   address: String! 
   symbol: String!
----
+  assetAddress: String!
   bridgeAssetName: String!
   bridgeAssetAddr: String!
   marketSize: BigDecimal!
@@ -22,16 +22,15 @@ id: ID!
 
 // event NewSilo(address indexed silo, address indexed asset, string name, string symbol);
 
-
 export function handleSiloCreated(event: NewSilo): void {
   log.info('New Silo Market created event, name: {}', [event.params.name.toString()])
   let silo = new Silo(event.params.silo.toHex());
   silo.name = event.params.name;
   silo.address = event.params.silo.toHexString();
   silo.symbol = event.params.symbol;
+  silo.assetAddress = event.params.asset.toHexString();
   silo.bridgeAssetName = 'ETH';
   silo.bridgeAssetAddr = '0x00E55830966aeCA7579f99e97e58Bb8b5150B034';  //KovanETH
-
   const defaultBigDec = new BigDecimal(new BigInt(0));
   silo.marketSize = defaultBigDec;
   silo.totalBorrowed = defaultBigDec;
